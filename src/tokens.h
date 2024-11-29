c6eb8c30827f7ddf544c681db1e031649969074a
Mounir <lm_melzi@esi.dz>
2024-11-30 00:55:44 +0100

    create enum for tokens

--- src/tokens.h
+++ src/tokens.h
@@ -1,76 +1,75 @@
-#ifndef TOKENS_H
-#define TOKENS_H
+#pragma once
 
-/* Mots-clés */
-#define LET 1
-#define RETURN 2
-#define WRITE 3
-#define READ 4
+typedef enum Token {
+    /* Mots-clés */
+    LET = 1,
+    RETURN,
+    WRITE,
+    READ,
 
-/* Types */
-#define TYPE_INT 5
-#define TYPE_FLOAT 6
-#define TYPE_STRING 7
-#define TYPE_BOOLEAN 8
-#define TYPE_CHAR 9
+    /* Types */
+    TYPE_INT,
+    TYPE_FLOAT,
+    TYPE_STRING,
+    TYPE_BOOLEAN,
+    TYPE_CHAR,
 
-/* Identifiants et Littéraux */
-#define IDENTIFIER 10
-#define INTEGER_LITERAL 11
-#define FLOAT_LITERAL 12
-#define STRING_LITERAL 13
-#define BOOLEAN_LITERAL 14
-#define CHAR_LITERAL 15
+    /* Identifiants et Littéraux */
+    IDENTIFIER,
+    INTEGER_LITERAL,
+    FLOAT_LITERAL,
+    STRING_LITERAL,
+    BOOLEAN_LITERAL,
+    CHAR_LITERAL,
 
-/* Tableaux */
-#define LBRACKET 16
-#define RBRACKET 17
-#define COMMA 18
+    /* Tableaux */
+    LEFT_BRACKET,
+    RIGHT_BRACKET,
+    COMMA,
 
-/* Enregistrements (Struct) */
-#define STRUCT_BEGIN 19
-#define STRUCT_END 20
+    /* Enregistrements (Struct) */
+    STRUCT_BEGIN,
+    STRUCT_END,
 
-/* Opérateurs */
-#define PLUS 21
-#define MINUS 22
-#define MULTIPLY 23
-#define DIVIDE 24
-#define MODULO 25  
-#define ASSIGN 26
-#define EQUAL 27
-#define NOT_EQUAL 28
-#define LESS 29          
-#define LESS_EQUAL 30    
-#define GREATER 31       
-#define GREATER_EQUAL 32 
-#define COLON 33
-#define LOGICAL_AND 34
-#define LOGICAL_OR 35
-#define NOT 36
+    /* Opérateurs */
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    MODULO,
+    ASSIGN,
+    EQUAL,
+    NOT_EQUAL,
+    LESS,
+    LESS_OR_EQUAL,
+    GREATER,
+    GREATER_OR_EQUAL,
+    COLON,
+    AND,
+    OR,
+    NOT,
 
-/* Parenthèses */
-#define LPAREN 37        
-#define RPAREN 38        
+    /* Parenthèses */
+    LEFT_PARENTHESIS,
+    RIGHT_PARENTHESIS,
 
-/* Symboles spéciaux */
-#define LBRACE 39
-#define RBRACE 40
-#define HTPL_BEGIN 41
-#define HTPL_END 42
-#define SEMICOLON 43
+    /* Symboles spéciaux */
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    HTPL_BEGIN,
+    HTPL_END,
+    SEMICOLON,
 
-/* Fonctions */
-#define FUNCTION_BEGIN 44
-#define FUNCTION_END 45         
-#define FUNCTION_NAME 46   
-#define MAIN 47
+    /* Fonctions */
+    FUNCTION_BEGIN,
+    FUNCTION_END,
+    FUNCTION_NAME,
+    MAIN,
 
-/* Boucles et conditions */
-#define IF_BEGIN 48  
-#define IF_END 49       
-#define ELSE 50   
-#define WHILE_BEGIN 51
-#define WHILE_END 52  
-
-#endif
+    /* Boucles et conditions */
+    IF_BEGIN,
+    IF_END,
+    ELSE,
+    WHILE_BEGIN,
+    WHILE_END
+} Token;
